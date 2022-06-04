59e1dbbb56c568891ee7916bc98f89a14fd23617
Fabio Barros <fabio.junior643@outlook.com>
2022-06-04 18:35:14 -0300

    change globalStyles

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -5,7 +5,12 @@ const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
     outline: 0;
-    box-sizing: border-box
+    box-sizing: border-box;
+    font-family: 'Work Sans', sans-serif;
+  }
+
+  html body #root {
+    height: 100vh;
   }
 `;
 
