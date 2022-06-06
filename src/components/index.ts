12f4bed910873224afa6a097e4ee66f24da32aff
Fabio Barros <fabio.junior643@outlook.com>
2022-06-05 21:25:06 -0300

    export pagewrapper

--- src/components/index.ts
+++ src/components/index.ts
@@ -5,3 +5,4 @@ export { default as TextField } from './input/textfield/TextField';
 export { default as Typography } from './typography/Typography';
 export { default as Form } from './form/Form';
 export { default as Header } from './header/Header';
+export { default as PageWrapper } from './pagewrapper/PageWrapper';
