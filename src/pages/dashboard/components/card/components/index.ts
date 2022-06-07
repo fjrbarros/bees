9506c612363789721fdbbebfa915e95d8c7b2698
Fabio Barros <fabio.junior643@outlook.com>
2022-06-07 17:31:56 -0300

    export component from page dashboard

---
+++ src/pages/dashboard/components/card/components/index.ts
@@ -0,0 +1,4 @@
+export { default as CardHeader } from './cardHeader/CardHeader';
+export { default as CardBody } from './cardBody/CardBody';
+export { default as CardTags } from './cardTags/CardTags';
+export { default as Tag } from './tag/Tag';
