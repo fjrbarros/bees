6add2128b306ea3357640d34e778b62aef024416
Fabio Barros <fabio.junior643@outlook.com>
2022-06-07 18:46:31 -0300

    util lower

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,3 @@
 export * from './validateUserName/validateUserName';
 export * from './test/test-utils';
+export * from './lower/lower';
---
+++ src/utils/lower/lower.test.ts
@@ -0,0 +1,8 @@
+import { lower } from './lower';
+
+describe('lower function', () => {
+  it('should return value to lower case', () => {
+    expect(lower('Test')).toEqual('test');
+    expect(lower('')).toEqual('');
+  });
+});
