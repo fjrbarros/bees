47358b5d9c517d4814b83a3caa360efbdd31df3f
Fabio Barros <fabio.junior643@outlook.com>
2022-06-06 21:10:23 -0300

    add toast provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import { Provider } from 'react-redux';
+import { ToastContainer } from 'react-toastify';
+import App from './App';
 import store from './store';
 import Theme from './styles/Theme';
+import 'react-toastify/dist/ReactToastify.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
@@ -15,5 +17,6 @@ root.render(
         <App />
       </Theme>
     </Provider>
+    <ToastContainer />
   </React.StrictMode>,
 );
