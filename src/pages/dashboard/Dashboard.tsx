c42ef3d1c7ba5496d4d3a71a5318380eeff4aba3
Fabio Barros <fabio.junior643@outlook.com>
2022-06-07 18:45:56 -0300

    filter by high light text

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,13 +1,16 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { toast } from 'react-toastify';
 import { PageWrapper, Loading } from '../../components';
 import { Grid, NoData, Card } from './components';
 import { useDispatch, useSelector } from 'react-redux';
 import { AppDispatch, RootState } from '../../store';
 import { fetchBreweries } from '../../api/brewery';
+import { IBrewery } from '../../store/brewery/types';
+import { lower } from '../../utils';
 
 const Dashboard: React.FC = () => {
   const dispatch: AppDispatch = useDispatch();
+  const [toHighLight, setToHighLight] = useState<string>('');
   const { loading, error, data } = useSelector(
     (data: RootState) => data.brewery,
   );
@@ -28,14 +31,31 @@ const Dashboard: React.FC = () => {
     return <Loading />;
   }
 
+  const afterChangeSearchInput = (value: string) => {
+    setToHighLight(value);
+  };
+
+  const filterBreweryByText = (brewery: IBrewery) => {
+    return (
+      lower(brewery.name).includes(lower(toHighLight)) ||
+      lower(brewery.city).includes(lower(toHighLight)) ||
+      lower(brewery.state).includes(lower(toHighLight)) ||
+      lower(brewery.country).includes(lower(toHighLight))
+    );
+  };
+
   return (
-    <PageWrapper>
+    <PageWrapper onChangeSearch={afterChangeSearchInput}>
       {!data.length ? (
         <NoData />
       ) : (
         <Grid>
-          {data.map(brewery => (
-            <Card key={brewery.id} brewery={brewery} />
+          {data.filter(filterBreweryByText).map(brewery => (
+            <Card
+              key={brewery.id}
+              brewery={brewery}
+              toHighLight={toHighLight}
+            />
           ))}
         </Grid>
       )}
