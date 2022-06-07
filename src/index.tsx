eaf852ac01b85ac33127bb7664755409e22619fe
Fabio Barros <fabio.junior643@outlook.com>
2022-06-07 17:29:38 -0300

    add user name reload page

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,9 @@ import App from './App';
 import store from './store';
 import Theme from './styles/Theme';
 import 'react-toastify/dist/ReactToastify.css';
+import { addUserName } from './store/user';
+
+store.dispatch(addUserName(localStorage.getItem('userName')));
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
@@ -16,7 +19,7 @@ root.render(
       <Theme>
         <App />
       </Theme>
+      <ToastContainer />
     </Provider>
-    <ToastContainer />
   </React.StrictMode>,
 );
