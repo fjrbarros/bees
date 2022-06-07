b1d8b0e45cb99b33132140b2974047dd8fd80067
Fabio Barros <fabio.junior643@outlook.com>
2022-06-07 17:30:32 -0300

    change component form

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -1,17 +1,25 @@
 import { SyntheticEvent } from 'react';
 import styled from 'styled-components';
 
-const StyledForm = styled.form`
+const StyledForm = styled('form')<Props>`
   display: flex;
-  flex-direction: column;
+  flex-direction: ${({ flexDirection }) => flexDirection || 'column'};
+  align-items: ${({ alignItems }) => alignItems || 'normal'};
 `;
 
 interface Props {
   children: React.ReactNode;
+  flexDirection?: string;
+  alignItems?: string;
   onSubmit?: (event: SyntheticEvent) => void;
 }
 
-const Form: React.FC<Props> = ({ children, onSubmit }) => {
+const Form: React.FC<Props> = ({
+  children,
+  flexDirection,
+  alignItems,
+  onSubmit,
+}) => {
   const handleSubmit = (event: SyntheticEvent) => {
     event.preventDefault();
     if (onSubmit) {
@@ -19,7 +27,15 @@ const Form: React.FC<Props> = ({ children, onSubmit }) => {
     }
   };
 
-  return <StyledForm onSubmit={handleSubmit}>{children}</StyledForm>;
+  return (
+    <StyledForm
+      onSubmit={handleSubmit}
+      flexDirection={flexDirection}
+      alignItems={alignItems}
+    >
+      {children}
+    </StyledForm>
+  );
 };
 
 export default Form;
